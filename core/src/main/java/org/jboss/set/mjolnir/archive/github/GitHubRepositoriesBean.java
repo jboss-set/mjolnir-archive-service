7d2ae5dc3287c54d5f2241a659ce6ab6ab1c5ae3
Tomas Hofman <thofman@redhat.com>
2022-07-21 11:54:55 +0200

    Make 3 attempts to retrieve list of user's repositories to archive

--- core/src/main/java/org/jboss/set/mjolnir/archive/github/GitHubRepositoriesBean.java
+++ core/src/main/java/org/jboss/set/mjolnir/archive/github/GitHubRepositoriesBean.java
@@ -2,10 +2,15 @@ package org.jboss.set.mjolnir.archive.github;
 
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.client.IGitHubConstants;
 import org.eclipse.egit.github.core.service.RepositoryService;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,7 +21,9 @@ import java.util.stream.Collectors;
  */
 public class GitHubRepositoriesBean {
 
-    final private RepositoryService repositoryService;
+    private static final Logger LOG = Logger.getLogger(GitHubRepositoriesBean.class);
+
+    private final RepositoryService repositoryService;
 
     @Inject
     public GitHubRepositoriesBean(GitHubClient client) {
@@ -31,22 +38,39 @@ public class GitHubRepositoriesBean {
      * @return list of private repositories
      */
     public Set<Repository> getRepositoriesToArchive(String organisation, String githubUser) throws IOException {
-        List<Repository> orgRepositories = repositoryService.getOrgRepositories(organisation);
+        SocketTimeoutException timeoutException = null;
 
-        List<Repository> privateRepositories = orgRepositories.stream()
-                .filter(Repository::isPrivate)
-                .collect(Collectors.toList());
+        for (int attempt = 0; attempt < 3; attempt++) {
+            try {
+                List<Repository> orgRepositories = repositoryService.getOrgRepositories(organisation);
 
-        Set<Repository> userRepositories = new HashSet<>();
-        for (Repository sourceRepository : privateRepositories) {
-            List<Repository> forks = repositoryService.getForks(sourceRepository);
-            forks.stream()
-                    .filter(fork -> githubUser.toLowerCase().equals(fork.getOwner().getLogin().toLowerCase()))
-                    .peek(repository -> repository.setSource(sourceRepository)) // set organization's repository as the source repository
-                    .forEach(userRepositories::add);
+                List<Repository> privateRepositories = orgRepositories.stream()
+                        .filter(Repository::isPrivate)
+                        .collect(Collectors.toList());
+
+                Set<Repository> userRepositories = new HashSet<>();
+                for (Repository sourceRepository : privateRepositories) {
+                    List<Repository> forks = repositoryService.getForks(sourceRepository);
+                    forks.stream()
+                            .filter(fork -> githubUser.toLowerCase().equals(fork.getOwner().getLogin().toLowerCase()))
+                            .peek(repository -> repository.setSource(sourceRepository)) // set organization's repository as the source repository
+                            .forEach(userRepositories::add);
+                }
+
+                return userRepositories;
+            } catch (SocketTimeoutException e) {
+                timeoutException = e;
+                // try to report GitHub API IP address, to make it possible to rule out egress firewall issue
+                try {
+                    InetAddress addr = InetAddress.getByName(IGitHubConstants.HOST_API);
+                    LOG.warnf(e, "GitHub API connection timeout. %s resolves to %s",
+                            IGitHubConstants.HOST_API, addr.getHostAddress());
+                } catch (UnknownHostException e2) {
+                    LOG.warnf(e, "Failed to resolve GitHub API host");
+                }
+            }
         }
-
-        return userRepositories;
+        throw timeoutException;
     }
 
 }
